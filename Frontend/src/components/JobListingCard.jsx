4285ff525ac7296a82666593e142afade3752bf2
Chirag Raj <itschiragraj2025@gmail.com>
2024-08-04 19:00:47 +0530

    Work or Less and more decriptio of job card and use react icons after installing react icons

--- Frontend/src/components/JobListingCard.jsx
+++ Frontend/src/components/JobListingCard.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { FaMapMarker } from 'react-icons/fa';
+import { useState } from 'react';
 
 const JobListingCard = () => {
+  // let description = job.description; // do this after api call
+  let description = "We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript...";
+
+  let temp = description;
+  
+  const [showFullDescription, setShowFullDescription] = useState(false);
+  if(!showFullDescription) {
+      temp = description.slice(0, 93) + "...";
+  }
   return (
     <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
@@ -10,8 +21,16 @@ const JobListingCard = () => {
               </div>
 
               <div className="mb-5">
-               We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript...
+               {temp}
               </div>
+
+              <button className='text-indigo-500 mb-5 hover:text-indigo-600' onClick={() => {
+                // We can dirctly paas value instead of function
+                setShowFullDescription((prevState) => {
+                  prevState = !prevState;
+                  return prevState;
+                })
+              }}>{showFullDescription ? "Less" : "More"}</button>
 
               <h3 className="text-indigo-500 mb-2">$70 - $80K / Year</h3>
 
@@ -19,7 +38,7 @@ const JobListingCard = () => {
 
               <div className="flex flex-col lg:flex-row justify-between mb-4">
                 <div className="text-orange-700 mb-3">
-                  <i className="fa-solid fa-location-dot text-lg"></i>
+                <FaMapMarker className='inline text-lg mb-1 mr-1' />
                   Boston, MA
                 </div>
                 <a
